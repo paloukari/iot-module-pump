46afb8d9eeba28d2280d1bcc2b0865378dc96f0f
Daniel Scholl <dascholl@microsoft.com>
2019-06-12 21:24:13 -0500

    Fixed Deploy

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+#  Purpose: Initialize the environment
+#  Usage:
+#    deploy.sh
+
+###############################
+## ARGUMENT INPUT            ##
+###############################
+usage() { echo "Usage: deploy.sh <iot_hub> <edge_vm>" 1>&2; exit 1; }
+
+if [ ! -z $1 ]; then IOT_HUB=$1; fi
+if [ -z $IOT_HUB ]; then
+  usage
+fi
+
+if [ ! -z $2 ]; then EDGE_VM=$2; fi
+if [ -z $EDGE_VM ]; then
+  usage
+fi
+
+echo "Deploying modules to ${EDGE_VM}"
+
+az iot edge set-modules \
+  --device-id ${EDGE_VM} \
+  --hub-name ${IOT_HUB} \
+  --content config/deployment.json
